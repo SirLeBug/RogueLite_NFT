1055ae1c02663e1b2b9b1ec93c4a5bda2801f8a9
DanielSanz_BitBucket <dani1sanz1frias@gmail.com>
2022-03-02 12:44:21 +0100

    Added walls, floor and colision with both npcs and walls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
-
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -34,8 +34,25 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        //Player movement
+        //Collision (y axis)
 
-        transform.Translate(moveDelta * Time.deltaTime);
+        hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(0,moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+           
+        if(hit.collider == null)
+        {
+            //Player movement (y axis)
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        //Collision (x axis)
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            //Player movement (x axis)
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+
     }
 }
