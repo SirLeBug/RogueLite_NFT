537640ddb7cd842b25025343f13ebc8f70774674
DanielSanz_BitBucket <dani1sanz1frias@gmail.com>
2022-03-02 12:10:51 +0100

    Changed the name of the player script from "PlayerScript" to Player and added mobility functionality aswell as a box collider on the player

---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    private Vector3 moveDelta;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    //We use FixedUpdate because its more friendly with physics (frame by frame) than regular Update
+    private void FixedUpdate()
+    {
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        //Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        //Swap sprite direction
+
+        if(moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        //Player movement
+
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+}
